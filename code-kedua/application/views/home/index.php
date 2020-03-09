6c2d615ecceafd2bd5782d8ed395df979348f7c9
Unero <20809454+Unero@users.noreply.github.com>
2020-03-09 09:25:18 +0700

    Jobsheet 6 Datatables dan DOMPDF

--- code-kedua/application/views/home/index.php
+++
@@ -1,7 +0,0 @@
-
-    <?php foreach ($nama as $mhs_name) {
-        ?>
-        <h1>Hello, <?php echo $mhs_name;?></h1>
-        <?php
-    } 
-    ?>
--- code-keempat/application/views/home/belajar.php
+++
@@ -1,1 +0,0 @@
-    <h1>Saya belajar CodeIgniter</h1>
